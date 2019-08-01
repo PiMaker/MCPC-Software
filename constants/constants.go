17091f6dc4fbd65ac81d02cfc4fbbb69ea55175f
PiMaker <stefan@pimaker.at>
2019-08-01 20:28:57 +0200

    Fix LICENSE and keycode lookup (fix letter q)

--- constants/constants.go
+++ constants/constants.go
@@ -5,7 +5,7 @@ Copyright (C) 2019 Stefan Reiter (pimaker.at)
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.
-See https://github.com/PiMaker/MCPC/blob/master/LICENSE for more.
+See https://github.com/PiMaker/MCPC-Software/blob/master/LICENSE for more.
 `
 
 const MCPCVersion string = "0.6"
